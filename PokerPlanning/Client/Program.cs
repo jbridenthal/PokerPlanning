ae2f80033bbe8fa8193f3a5b7a5b44897e2fa184
James Bridenthal <jamesbridenthal@gmail.com>
2022-03-21 21:19:52 -0400

    initial add local storage for refresh

--- PokerPlanning/Client/Program.cs
+++ PokerPlanning/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
@@ -9,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 builder.Services.AddMudServices(config =>
 {
