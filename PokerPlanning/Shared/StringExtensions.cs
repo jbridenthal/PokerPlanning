be897ec7f4817c6f8055f2235e122be8e92ac067
James Bridenthal <jamesbridenthal@gmail.com>
2022-11-01 19:35:27 -0400

    Added check for if Vote was Numeric

--- PokerPlanning/Shared/StringExtensions.cs
+++ PokerPlanning/Shared/StringExtensions.cs
@@ -6,5 +6,10 @@ namespace PokerPlanning.Shared
         {
             return int.TryParse(toParse, out var result) ? result : 0;
         }
+
+        public static bool IsNumeric(this string toParse)
+        {
+            return int.TryParse(toParse, out _);
+        }
     }
 }
